5056e8226e2557759a24f59a27f70eef943d9982
Reece Humphreys <reecewh@icloud.com>
2023-10-21 17:22:40 -0400

    Update to Kessler 0.1.4

--- examples/example.py
+++ examples/example.py
@@ -2,15 +2,23 @@ import numpy as np
 from kesspy import Satellite, ExplosionEvent, run_explosion
 
 pos = np.array([0.0, 0.0, 0.0], np.float32)
-vel = np.array([0.0, 0.0, 0, 0], np.float32)
+vel = np.array([0.0, 0.0, 0.0], np.float32)
 
-sat = Satellite(pos, vel, 839.0, 0.000000000000001)
-event = ExplosionEvent(sat)
+sat = Satellite(pos, vel, 839.0)
+event = ExplosionEvent(sat, 0.1)
 debris = run_explosion(event)
 
-mean_area = np.mean(debris[:, 4, 0])
-mean_mass = np.mean(debris[:, 5, 0])
+rs = debris[:, 1, :].astype(np.float64)  # km
+vs = debris[:, 6, :].astype(np.float64)  # km/s
+char_les = debris[:, 2, :].astype(np.float64)  # m
+ams = debris[:, 3, :].astype(np.float64)  # kg
+areas = debris[:, 4, :].astype(np.float64)  # m^2
+masses = debris[:, 5, :].astype(np.float64)  # kg
 
-print(f"{debris.shape[0]} Pieces of debris generated.")
-print(f"Mean mass: {mean_mass}")
-print(f"Mean area: {mean_area}")
+print("Average Characteristic len.:", np.mean(char_les[:, 0]))
+print("Average A/M:", np.mean(ams[:, 0]))
+print("Average Area:", np.mean(areas[:, 0]))
+print("Average Mass:", np.mean(masses[:, 0]))
+
+vel_mags = np.linalg.norm(vs, axis=1)
+print("Average vel mag:", np.mean(vel_mags))
--- src/lib.rs
+++ src/lib.rs
@@ -14,9 +14,10 @@ impl CollisionEventPyWrapper {
         _py: Python,
         satellite_one: &SatellitePyWrapper,
         satellite_two: &SatellitePyWrapper,
+        min_characteristic_length: f32,
     ) -> Self {
         let satellites = &[satellite_one.0.to_owned(), satellite_two.0.to_owned()];
-        CollisionEventPyWrapper(CollisionEvent::new(satellites))
+        CollisionEventPyWrapper(CollisionEvent::new(satellites, min_characteristic_length))
     }
 }
 
@@ -27,9 +28,9 @@ pub struct ExplosionEventPyWrapper(ExplosionEvent);
 #[pymethods]
 impl ExplosionEventPyWrapper {
     #[new]
-    pub fn __new__(_py: Python, satellite: &SatellitePyWrapper) -> Self {
+    pub fn __new__(_py: Python, satellite: &SatellitePyWrapper, min_characteristic_length: f32) -> Self {
         let satellite = satellite.0.to_owned();
-        ExplosionEventPyWrapper(ExplosionEvent::new(satellite))
+        ExplosionEventPyWrapper(ExplosionEvent::new(satellite, min_characteristic_length))
     }
 }
 
@@ -45,17 +46,39 @@ impl SatellitePyWrapper {
         position: &PyArray1<f32>,
         velocity: &PyArray1<f32>,
         mass: f32,
-        characteristic_length: f32,
     ) -> Self {
-        let position = unsafe { position.as_array().to_owned() }; // Convert to ndarray type
-        let velocity = unsafe { velocity.as_array().to_owned() }; // Convert to ndarray type
-        SatellitePyWrapper(Satellite {
-            position,
-            velocity,
-            mass,
-            sat_kind: SatKind::Rb, // TODO
-            characteristic_length,
-        })
+
+        let position: [f32; 3] = {
+            unsafe {
+                let position_slice = position.as_slice().expect("Failed to get slice from position array");
+                if position_slice.len() != 3 {
+                    panic!("Position array must have a length of 3");
+                }
+                let mut arr = [0.0; 3];
+                arr.copy_from_slice(position_slice);
+                arr
+            }
+        };
+
+        let velocity: [f32; 3] = {
+            unsafe {
+                let velocity_slice = velocity.as_slice().expect("Failed to get slice from velocity array");
+                if velocity_slice.len() != 3 {
+                    panic!("Velocity array must have a length of 3");
+                }
+                let mut arr = [0.0; 3];
+                arr.copy_from_slice(velocity_slice);
+                arr
+            }
+        };
+        SatellitePyWrapper(
+            Satellite::new(
+                position,
+                velocity,
+                mass,
+                SatKind::Rb //TODO
+            )
+        )
     }
 }
 
